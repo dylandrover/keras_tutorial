46602d376a3a7a05a2e7af9a0e76598e590a9ce9
dylandrover <dylandrover@gmail.com>
2015-12-02 01:03:21 -0500

    fixing install_script

--- keras_tutorial/install_script.sh
+++ keras_tutorial/install_script.sh
@@ -3,13 +3,12 @@
 sudo apt-get update
 
 #Install all the python tools and fun other bits
-sudo apt-get -y install libhdf5-8 libhdf5-dev libhdf5-cpp-8 python-h5py python2.7 python-numpy python-scipy python-dev g++ python-sklearn libblas3 libblas-common
-
+sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git libhdf5-serial-dev
 #install the python package manager pip
 sudo apt-get -y install python-pip
 sudo pip install --upgrade pip
 
 #Use pip to install theano and keras
-sudo pip install pyyaml 
+sudo pip install pyyaml h5py
 sudo pip install git+git://github.com/Theano/Theano.git
 sudo pip install keras
